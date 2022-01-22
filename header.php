72d96256fd73d3c0e53303e927b00cf749b36d16
Brandon Chow <53826472+kungfumaster731@users.noreply.github.com>
2022-01-22 11:54:20 -0500

    d

---
+++ header.php
@@ -0,0 +1,5 @@
+<h1>Brandon's Website</h1>
+<hr>
+<h2> <?php echo $title; ?></h2>
+<h3> <?php echo $author; ?></h3>
+Word count: <?php echo $wordCount; ?>
--- site8.php
+++ site8.php
@@ -5,9 +5,38 @@
     <title>Site 8</title>
   </head>
   <body>
-    <?php include "header.html" ?>
 
+    <h1>Include Method</h1>
 
-    <?php include "footer.html" ?>
+    <?php $title = "My First Post";
+    $author = "Brandon";
+    $wordCount = 400;
+    include "header.php";
+    $wordCount = 1000;
+    echo $wordCount;
+    ?>
+
+    <?php include "footer.php" ?>
+    <h1>Function</h1>
+
+    <?php
+    include "randomFunc.php";
+    echo $feetInMile;
+     ?>
+     <h1>Class and Object</h1>
+
+     <?php
+      class Book{
+        var $title;
+        var $author;
+        var $pages;
+      }
+
+      $book1 = new Book;
+      $book1->title = "Harry Potter";
+      $book1->author = "JK Rowling";
+      $book->pages=400;
+      ?>
+
   </body>
 </html>
