c15be0c0ec1741101291494d4f9be138fefeb350
Brandon Chow <53826472+kungfumaster731@users.noreply.github.com>
2022-01-23 18:33:09 -0500

    d

--- site8.php
+++ site8.php
@@ -31,24 +31,21 @@
         var $title;
         var $author;
         var $pages;
+
+        function __construct($aName, $aTitle, $aAuthor, $aPages){
+          $this->name = $aName;
+          $this->title = $aTitle;
+          $this->author = $aAuthor;
+          $this->pages = $aPages;
+        }
       }
 
-      $book1 = new Book;
-      $book1->name = "Book 1";
-      $book1->title = "Harry Potter";
-      $book1->author = "JK Rowling";
-      $book1->pages = 400;
-
+      $book1 = new Book("Book 1", "Harry Potter", "JK Rowling", 400);
+      $book2 = new Book("Book 2", "Lord Of The Rings", "Tolkien", 700);
       echo "<h3>", $book1->name, "</h3>", "<br>";
       echo $book1->title, "<br>";
       echo $book1->author, "<br>";
       echo $book1->pages, "<br>";
-
-      $book2 = new Book;
-      $book2->name = "Book 2";
-      $book2->title = "Brandon Is Awesome";
-      $book2->author = "Brandon";
-      $book2->pages = 1;
 
       echo "<h3>", $book2->name, "</h3>", "<br>";
       echo $book2->title, "<br>";
@@ -57,5 +54,91 @@
 
       ?>
 
+      <h1>Object Functions</h1>
+
+      <?php
+        class Student{
+          var $name;
+          var $major;
+          var $gpa;
+
+          function __construct($name, $major, $gpa){
+            $this->name = $name;
+            $this->major=$major;
+            $this->gpa=$gpa;
+          }
+
+          function hasHonors(){
+            if($this->gpa >= 3.5){
+              return "true <br>";
+            }
+
+            return "false <br>";
+          }
+        }
+
+        $student1 = new Student("Brandon", "Computer Engineering", 3.9);
+        $student2 = new Student("Daniel", "Plumbing", 1);
+        $student3 = new Student("Celena", "Breathing", 0.1);
+
+        echo $student1->hasHonors();
+        echo $student2->hasHonors();
+        echo $student3->hasHonors();
+
+       ?>
+
+       <h1>Challenge: Anime Class</h1>
+
+       <?php
+       // creat an anime class that will take three variables: name, score(0,10), gmp_legendre
+       // create a constructor that overwrites those three variables from the user inputs
+       // create a custom function for checking the score, whether it is bad, okay, or amazing
+       // output everything including all the variables and score checking results
+        class Anime{
+         var $name;
+         var $genre;
+         var $score;
+         function __construct($name, $genre, $score){
+           $this->name = $name;
+           $this->genre = $genre;
+           $this->score = $score;
+         }
+
+         function isGood(){
+           if($this->score >= 8){
+             return "Very Good <br>";
+           }
+           elseif($this->score >= 5){
+             return "Okay <br>";
+           }
+           else{
+             return "Bad <br>";
+           }
+         }
+        }
+
+
+
+        $anime1 = new Anime("Sword Art Online", "Isekai", 9);
+        $anime2 = new Anime("Attack On Titan", "Shonen", 7);
+        $anime3 = new Anime("Magi", "Adventure", 8);
+
+
+
+        echo $anime1->name, ", ";
+        echo $anime1->genre, ", ";
+        echo $anime1->score, ", ";
+        echo $anime1->isGood(), "<br>";
+
+        echo $anime2->name, ", ";
+        echo $anime2->genre, ", ";
+        echo $anime2->score, ", ";
+        echo $anime2->isGood(), "<br>";
+
+        echo $anime3->name, ", ";
+        echo $anime3->genre, ", ";
+        echo $anime3->score, ", ";
+        echo $anime3->isGood(), "<br>";
+        ?>
   </body>
 </html>
